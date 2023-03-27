3bc8f7519787bcd82120a1d03947d0f9c744e06f
Roberto-93 <roberto.carfi@stevejobs.academy>
2023-03-27 12:08:07 +0200

    primo commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+//ciao hello world.
+const add = (a:number, b:number) => a + b ;
+
+const minus = (a:number, b:number) => a - b ;
+
+const grnericfunction = (a: number, b:number, type:"+"|"-")=> type === "-" ? a+b: a - b
